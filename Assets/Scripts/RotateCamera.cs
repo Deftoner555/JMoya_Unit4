f43da1839973a8a9f6b5945b56a8cf2636e3a23c
Deftoner555 <jmoya555@gmail.com>
2023-10-24 12:09:51 -0700

    Finished Lesson A

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float rotateInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, rotateInput * speed * Time.deltaTime);
+    }
+}
