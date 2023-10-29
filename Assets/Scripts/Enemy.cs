bf802e5b54772527378003feff1b5fee25a09908
Deftoner555 <jmoya555@gmail.com>
2023-10-29 12:56:35 -0700

    Finished Lesson B

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody enemyRB;
+    GameObject player;
+    public float speed = 4.0f;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 seekDirection = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce(seekDirection * speed * Time.deltaTime);
+    }
+}
