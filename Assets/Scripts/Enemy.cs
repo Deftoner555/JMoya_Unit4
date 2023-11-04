b76d52914ddb3b2a7422cd600454f4bf4bf149a9
Deftoner555 <jmoya555@gmail.com>
2023-11-04 00:25:50 -0700

    Finished Lesson D

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MonoBehaviour
     GameObject player;
     public float speed = 4.0f;
 
+    private float yBoundary = -15.0f;
+
     void Start()
     {
         player = GameObject.Find("Player");
@@ -17,6 +19,11 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        if(transform.position.y < yBoundary)
+        {
+            Destroy(gameObject);
+        }
+
         Vector3 seekDirection = (player.transform.position - transform.position).normalized;
         enemyRB.AddForce(seekDirection * speed * Time.deltaTime);
     }
